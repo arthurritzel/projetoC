f71a5b29ecb0983ef2d3fc6c0596a29ae1e3ea87
João Vitor Dysarz <106770829+KronosZbr@users.noreply.github.com>
2023-11-17 20:11:34 -0300

    Alteração na função de visualizar_arvore

--- src/visualizar.h
+++ src/visualizar.h
@@ -2,7 +2,7 @@
 void visualizar_arvore(Evento raiz) {
     if (raiz != NULL) {
         visualizar_arvore(raiz->esquerda);
-        printf("Data: %d/%d - Descricao: %s\n", raiz->dia, raiz->mes, raiz->descricao);
+        printf("Dia: %d/ mes:%d \n - Descricao: %s\n------------\n", raiz->dia, raiz->mes, raiz->descricao);
         visualizar_arvore(raiz->direita);
     }
 }
