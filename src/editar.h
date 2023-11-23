be15d0ad8f12f421cee5a7dc398931000a135408
arthurritzel <ritzelarthur@gmail.com>
2023-11-23 00:18:05 -0300

    editar

--- src/editar.h
+++ src/editar.h
@@ -1,13 +1,8 @@
-
-
-
-
 void editar_no(no *no_print, int dia, int mes, char desc[100], no *ant){
     int confirma = 0;
     if(no_print == NULL){
         return;
     }else{
-        
         editar_no(no_print->esquerda, dia, mes, desc, no_print);
         editar_no(no_print->direita, dia, mes, desc, no_print);
         if(no_print->dia == dia && no_print->mes == mes){
